cb142d6bc6ef97af3ac0775e68e357719020755f
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-02-13 23:00:35 +0300

    Добавлен интерактивный счётчик, теперь перерендериваться будет только одна карточка

--- static/bouquet.js
+++ static/bouquet.js
@@ -1,36 +1,43 @@
+// Рендер в самом начале
+let localHTML = "";
+for (let flower of flowers) {
+    localHTML += `
+        <a class="flower_card flower_card_mini" id="flower_card_${flower.title.replace(' ', '_')}">
+            <h4>${flower.title}</h4>
+            <img src="${"static/" + flower.link}" alt="${flower.title}">
+            <hr>
+            <button onclick="changeFlowerInBouquet('${flower.title}'); updateCard('${flower.title}'); updateCost()">Добавить</button>
+        </a>`;
+}
+document.querySelector("#scroll_constructor_list").innerHTML = localHTML;
+
 // Генерация конструктора цветов
 let bouquet = [];
-function renderConstructor() {
-    let globalHTML = "";
-    let localHTML = "";
+
+
+function updateCost() {
     let cost = 0;
-    for (let flower of flowers) {
-        let isActive = bouquet.indexOf(flower.title) !== -1;
-        if (isActive){
-            cost += flower.price;
+    for (let flowerTitle of bouquet) {
+        if (flowerTitle === undefined){
+            continue;
         }
-        localHTML += `
+        cost += flowers.find(e => e.title === flowerTitle.replace('_', ' ')).price;
+    }
+    document.querySelector("#constructor_cost").innerHTML = cost ? `Итого: ${cost} Рублей` : "";
+}
+
+function updateCard(title) {
+    let flower = flowers.find(e => e.title === title);
+    let isActive = bouquet.indexOf(title) !== -1;
+    document.querySelector(`#flower_card_${flower.title.replace(' ', '_')}`).innerHTML = `
         <a class="flower_card flower_card_mini" id="flower_card_${flower.title}" style="${isActive ? '' : 'opacity:0.8;'}">
             <h4>${flower.title}</h4>
             <img src="${"static/" + flower.link}" alt="${flower.title}">
             <hr>
-            <button onclick="changeFlowerInBouquet('${flower.title}'); renderConstructor();">${!isActive ? 'Добавить' : 'Убрать'}</button>
+            <button onclick="changeFlowerInBouquet('${flower.title}'); updateCard('${flower.title}'); updateCost()">${!isActive ? 'Добавить' : 'Убрать'}</button>
         </a>`;
-    }
-    globalHTML += `
-        <div class="scroll_assort" id="scroll_constructor_list">
-            ${localHTML}
-        </div>
-        <h4 id="constructor_cost">${cost ? `Итого: ${cost} Рублей` : ""}</h4>
-    `
-    if (cost){
-        globalHTML += `<div>
-        <button style="width: 120px">Заказать</button>`
-    }
-    document.querySelector("#constructor__inner").innerHTML = globalHTML;
 }
 
-renderConstructor();
 
 function changeFlowerInBouquet(title) {
     if (bouquet.indexOf(title) >= 0) {
