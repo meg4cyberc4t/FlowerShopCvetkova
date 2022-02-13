dc43213ceaa1e32be6027d538aa1af2c04dcc026
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-02-13 19:03:30 +0300

    Добавлены новые цветы, ассортимент немного доработан

---
+++ static/assort.js
@@ -0,0 +1,10 @@
+// Генерация ассортимента
+for (let flower of flowers){
+    document.querySelector("#scroll_assort_list").innerHTML += `
+    <a class="flower_card" id="flower_card_${flower.title}">
+            <h4>${flower.title}</h4>
+            <img src="${"static/" + flower.link}" alt="${flower.title}">
+            <h6>${flower.price}р/шт</h6>
+            <button>Заказать</button>
+        </a>`;
+}
---
+++ static/flowers.js
@@ -0,0 +1,183 @@
+const flowers = [
+    {
+        type: "gerberas",
+        title: "Голубые герберы",
+        link: "assets/flowers/gerberas/blue.jpg",
+        price: 79,
+    },
+    {
+        type: "gerberas",
+        title: "Телесные герберы",
+        link: "assets/flowers/gerberas/flesh.jpg",
+        price: 79,
+    },
+    {
+        type: "gerberas",
+        title: "Оранжевые герберы",
+        link: "assets/flowers/gerberas/orange.jpg",
+        price: 79,
+    },
+    {
+        type: "gerberas",
+        title: "Розовые герберы",
+        link: "assets/flowers/gerberas/pink.jpg",
+        price: 79,
+    },
+    {
+        type: "gerberas",
+        title: "Белые герберы",
+        link: "assets/flowers/gerberas/white.jpg",
+        price: 79,
+    },
+
+    {
+        type: "grass",
+        title: "Голубая душица",
+        link: "assets/flowers/grass/blue.jpg",
+        price: 20,
+    },
+    {
+        type: "grass",
+        title: "Ромашки",
+        link: "assets/flowers/grass/daisies.jpg",
+        price: 20,
+    },
+    {
+        type: "grass",
+        title: "Кустарная трава",
+        link: "assets/flowers/grass/hard_grass.jpg",
+        price: 15,
+    },
+    {
+        type: "grass",
+        title: "Классическая душица",
+        link: "assets/flowers/grass/oregano.jpg",
+        price: 10,
+    },
+    {
+        type: "grass",
+        title: "Травушка",
+        link: "assets/flowers/grass/real_grass.jpg",
+        price: 10,
+    },
+
+    {
+        type: "lilies",
+        title: "Красные лилии",
+        link: "assets/flowers/lilies/red.jpg",
+        price: 50,
+    },
+    {
+        type: "lilies",
+        title: "Белокрасные лилии",
+        link: "assets/flowers/lilies/red_yellow.jpg",
+        price: 50,
+    },
+    {
+        type: "lilies",
+        title: "Белые лилии",
+        link: "assets/flowers/lilies/white.jpg",
+        price: 50,
+    },
+
+    {
+        type: "pions",
+        title: "Нежные розовые",
+        link: "assets/flowers/pions/dear_pink.jpg",
+        price: 100,
+    },
+    {
+        type: "pions",
+        title: "Розовые",
+        link: "assets/flowers/pions/pink.jpg",
+        price: 80,
+    },
+
+    {
+        type: "tuples",
+        title: "Бежевые тюльпаны",
+        link: "assets/flowers/tuples/beige.jpg",
+        price: 70,
+    },
+    {
+        type: "tuples",
+        title: "Тёмноголубые тюльпаны",
+        link: "assets/flowers/tuples/darkblue.jpg",
+        price: 70,
+    },
+    {
+        type: "tuples",
+        title: "Красные тюльпаны",
+        link: "assets/flowers/tuples/red.jpg",
+        price: 80,
+    },
+
+    {
+        type: "roses_big",
+        title: "Бежевые розы",
+        link: "assets/flowers/roses/big/beige.png",
+        price: 100,
+    },
+    {
+        type: "roses_big",
+        title: "Телесные розы",
+        link: "assets/flowers/roses/big/flesh.jpg",
+        price: 100,
+    },
+    {
+        type: "roses_big",
+        title: "Красные розы",
+        link: "assets/flowers/roses/big/red.gif",
+        price: 100,
+    },
+    {
+        type: "roses_big",
+        title: "Рыжие розы",
+        link: "assets/flowers/roses/big/red.png",
+        price: 100,
+    },
+    {
+        type: "roses_big",
+        title: "Белые розы",
+        link: "assets/flowers/roses/big/white.png",
+        price: 100,
+    },
+    {
+        type: "roses_big",
+        title: "Белосерые розы",
+        link: "assets/flowers/roses/big/white_grey.jpg",
+        price: 100,
+    },
+    {
+        type: "roses_big",
+        title: "Белокрутые розы",
+        link: "assets/flowers/roses/big/white_happy.jpg",
+        price: 100,
+    },
+
+    {
+        type: "roses_little",
+        title: "Тёмные розочки",
+        link: "assets/flowers/roses/little/black.jpg",
+        price: 110,
+    },
+    {
+        type: "roses_little",
+        title: "Голубые розочки",
+        link: "assets/flowers/roses/little/blue.jpg",
+        price: 110,
+    },
+    {
+        type: "roses_little",
+        title: "Красные розочки",
+        link: "assets/flowers/roses/little/red.jpg",
+        price: 110,
+    },
+    {
+        type: "roses_little",
+        title: "Белые розочки",
+        link: "assets/flowers/roses/little/white.jpg",
+        price: 110,
+    },
+];
+
